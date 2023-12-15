8f78ce8bf308728dc53dea28697dcf6236814ef3
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2023-12-14 22:06:43 -0500

    added javascript that allows function of password generator

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var length = prompt("How long would you like your password? (please choose a length between 8-128 characters)");
+
+function generatePassword() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+  
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+  
+      const randomChar = characters[randomIndex];
+  
+      password += randomChar;
+    }
+  
+    return password;
+  }
+  const newPassword = generatePassword(length);
+  console.log(newPassword)
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
