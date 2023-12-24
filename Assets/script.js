4432600e154c72901cc0364119a4f6282a78621d
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2023-12-24 12:35:10 -0500

    added function to write pssword and finalized notation

--- Assets/script.js
+++ Assets/script.js
@@ -5,58 +5,65 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
 
   // Calls the generatePassword function - this is the function responsible for password creation
-  function generatePassword(){
+  function generatePassword() {
 
     //This line determines the length of the password
-    const length = prompt("How long would you like your password? (please choose a length between 8-128 characters)");
+    const lengthRequest = prompt("How long would you like your password? (please choose a length between 8-128 characters)");
     /*this is a conditional statement - if the password request is not between 8-128 characters,
     it will start the function over until parameters are met*/
-    if (length < 8 || length > 128) {
+    if (lengthRequest < 8 || lengthRequest > 128) {
       alert("Your password must be between 8-128 characters!");
-      return generatePassword ();
+      return generatePassword();
     };
 
     //Series of prompts which will confirm with the user the characters to be included in the password
-    const upperCaseRequest = confirm (" Would you like your password to include uppercase letters?");
-    const lowerCaseRequest = confirm ("Would you like your password to include lowercase letters?");
-    const numericRequest = confirm ("Would you like your password to include numbers?");
-    const symbolsRequest = confirm ("Would you like your password to include special characters?");
+    const upperCaseRequest = confirm(" Would you like your password to include uppercase letters?");
+    const lowerCaseRequest = confirm("Would you like your password to include lowercase letters?");
+    const numericRequest = confirm("Would you like your password to include numbers?");
+    const symbolsRequest = confirm("Would you like your password to include special characters?");
 
     //Conditional statement that ensures user uses at least on character type 
-    if (upperCaseRequest === false && lowerCaseRequest === false && numericRequest === false 
+    if (upperCaseRequest === false && lowerCaseRequest === false && numericRequest === false
       && symbolsRequest === false) {
       alert("Your password must include at least one character type!");
-      return generatePassword (); 
-      };
+      return generatePassword();
+    };
 
     //Names the variables which are to be possible in charSet
     const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowerCase = "abcdefghijklmnopqrstuvwxyz";
     const numeric = "1234567890";
     const symbols = "!@#$%^&*?+=-";
-  
+
     let charSet = "";
-  
-    //Conditional statement that adds the user's requests of characters into the charset  
+
+    //Conditional statement that adds the user's requests of characters to be included into charSet  
     if (upperCaseRequest === true) {
       charSet += upperCase;
     } if (lowerCaseRequest === true) {
       charSet += lowerCase;
     } if (numericRequest === true) {
       charSet += numeric;
-    } if ( symbolsRequest === true) {
+    } if (symbolsRequest === true) {
       charSet += symbols
     };
 
 
-    console.log(charSet)
-    //let password = "";
-  
-  
-  
+    let password = "";
+
+    /*Produces password by making index based on the lengthRequest provided by user, randomIndex selects
+    a random number based on charSet length to ensure all characters specified by user are used, randomChar
+    selects characters randomly based on charSet and randomIndex and then adds them to password*/
+    for (let i = 0; i < lengthRequest; i++) {
+      const randomIndex = Math.floor(Math.random() * charSet.length);
+      const randomChar = charSet[randomIndex];
+      password += randomChar;
+    }
+
+    return password;
   };
 
-
+  //Writes password to the document by calling generate password function
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
