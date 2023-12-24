215d6f586c5d5bc1d11e2ca7c548b6111e6b0411
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2023-12-24 11:30:40 -0500

    revising code - wrapped generate password into write password, named more variables to be included in password and created prompts for the user to confirm, created conditional statesment that will add user requests into charSet

--- Assets/script.js
+++ Assets/script.js
@@ -1,30 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var length = prompt("How long would you like your password? (please choose a length between 8-128 characters)");
-
-function generatePassword() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
-  
-    let password = '';
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-  
-      const randomChar = characters[randomIndex];
-  
-      password += randomChar;
-    }
-  
-    return password;
-  }
-  const newPassword = generatePassword(length);
-  console.log(newPassword)
-
 // Write password to the #password input
 function writePassword() {
+
+  // Calls the generatePassword function - this is the function responsible for password creation
+  function generatePassword(){
+
+    //This line determines the length of the password
+    const length = prompt("How long would you like your password? (please choose a length between 8-128 characters)");
+    /*this is a conditional statement - if the password request is not between 8-128 characters,
+    it will start the function over until parameters are met*/
+    if (length < 8 || length > 128) {
+      alert("Your password must be between 8-128 characters!");
+      return generatePassword ();
+    };
+
+    //Series of prompts which will confirm with the user the characters to be included in the password
+    const upperCaseRequest = confirm (" Would you like your password to include uppercase letters?");
+    const lowerCaseRequest = confirm ("Would you like your password to include lowercase letters?");
+    const numericRequest = confirm ("Would you like your password to include numbers?");
+    const symbolsRequest = confirm ("Would you like your password to include special characters?");
+
+    //Names the variables which are to be possible in charSet
+    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    const numeric = "1234567890";
+    const symbols = "!@#$%^&*?+=-";
+  
+    let charSet = "";
+  
+    //Conditional statement that adds the user's requests of characters into the charset  
+    if (upperCaseRequest === true) {
+      charSet += upperCase;
+    } if (lowerCaseRequest === true) {
+      charSet += lowerCase;
+    } if (numericRequest === true) {
+      charSet += numeric;
+    } if ( symbolsRequest === true) {
+      charSet += symbols
+    };
+
+
+    console.log(charSet)
+    //let password = "";
+  
+  
+  
+  };
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
 
   passwordText.value = password;
 
